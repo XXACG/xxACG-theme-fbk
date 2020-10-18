6d01ae91d5fb1762dd6eff49d62f6561101f01f6
boxlab <boxlab@foxmail.com>
2020-10-19 00:39:19 +0800

    :fish:

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <vs-row>
+    <vs-row style="display: flex; justify-content: center;">
       <vs-col lg="8" xs="12">
 
         <div class="table-card">
@@ -13,30 +13,36 @@
                 <vs-th sort @click="users = $vs.sortData($event ,users, 'name')">游戏名称</vs-th>
                 <vs-th sort @click="users = $vs.sortData($event ,users, 'producer')">厂商</vs-th>
                 <vs-th sort @click="users = $vs.sortData($event ,users, 'date')">发行时间</vs-th>
+                <vs-th sort @click="users = $vs.sortData($event ,users, 'catalog')">类别</vs-th>
+                <vs-th></vs-th>
               </vs-tr>
             </template>
             <template #tbody>
               <vs-tr :key="i" v-for="(item, i) in $vs.getPage($vs.getSearch(users, search), page, max)"
                      style="height: 50px">
-                <vs-td>{{ item.name }}</vs-td>
+                <vs-tooltip bottom>
+                  <vs-td>{{ item.name }}</vs-td>
+                  <template #tooltip>
+                    <img width="100%" style="border-radius: 10px;" :src="item.cover" alt="">
+                  </template>
+                </vs-tooltip>
                 <vs-td>{{ item.producer }}</vs-td>
                 <vs-td>{{ item.date }}</vs-td>
-                <template #expand>
-                  <div class="gal-detail">
-                    <div>
-                      <img :src="item.cover" height="35px" alt="">
-                      <p>{{ item.name }}</p>
-                    </div>
-                    <div>
-                      <vs-button animation-type="scale" flat>
-                        <ion-icon name="cloud-download-outline"></ion-icon>
-                        <template #animate>
-                          下载
-                        </template>
-                      </vs-button>
-                    </div>
-                  </div>
-                </template>
+                <vs-td>{{ item.catalog }}</vs-td>
+                <vs-td style="display: flex; flex-direction: row">
+                  <vs-button style="min-width: 40px" animation-type="scale">
+                    <ion-icon name="information-circle-outline"></ion-icon>
+                    <template #animate>
+                      详情
+                    </template>
+                  </vs-button>
+                  <vs-button style="min-width: 40px" animation-type="scale" flat>
+                    <ion-icon name="cloud-download-outline"></ion-icon>
+                    <template #animate>
+                      下载
+                    </template>
+                  </vs-button>
+                </vs-td>
               </vs-tr>
             </template>
             <template #footer>
@@ -45,16 +51,6 @@
           </vs-table>
         </div>
 
-      </vs-col>
-      <vs-col lg="4" xs="12">
-        <vs-row>
-          <vs-col :key="index" v-for="(block, index) in 3" lg="4" xs="12">
-            <div
-              style="width: 100%; height: 100%; padding: 50px 25px; background-color: rgba(var(--vs-dark), 1); color: #e6e9ea;">
-              BLOCK
-            </div>
-          </vs-col>
-        </vs-row>
       </vs-col>
     </vs-row>
   </div>
@@ -69,11 +65,61 @@ export default {
     search: '',
     users: [
       {
-        "name": "Lorem ipsum",
-        "producer": "Yuzi-soft",
-        "date": "2019年",
-        "cover": "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
-      }
+        name: "Lorem ipsum",
+        producer: "Yuzi-soft",
+        date: "2019年",
+        catalog: "none",
+        cover: "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      },
+      {
+        name: "Lorem ipsum",
+        producer: "Yuzi-soft",
+        date: "2019年",
+        catalog: "none",
+        cover: "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      },
+      {
+        name: "Lorem ipsum",
+        producer: "Yuzi-soft",
+        date: "2019年",
+        catalog: "none",
+        cover: "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      },
+      {
+        name: "Lorem ipsum",
+        producer: "Yuzi-soft",
+        date: "2019年",
+        catalog: "none",
+        cover: "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      },
+      {
+        name: "Lorem ipsum",
+        producer: "Yuzi-soft",
+        date: "2019年",
+        catalog: "none",
+        cover: "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      },
+      {
+        name: "Lorem ipsum",
+        producer: "Yuzi-soft",
+        date: "2019年",
+        catalog: "none",
+        cover: "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      },
+      {
+        name: "Lorem ipsum",
+        producer: "Yuzi-soft",
+        date: "2019年",
+        catalog: "none",
+        cover: "https://picbed-1253315888.file.myqcloud.com/images/bg1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      },
     ]
   })
 }
@@ -91,24 +137,4 @@ export default {
   transition: all .25s ease;
   height: auto;
 }
-
-.gal-detail,
-.gal-detail > div {
-  display: flex;
-  align-items: center;
-}
-
-.gal-detail {
-  width: 100%;
-  justify-content: space-between;
-}
-
-.gal-detail > div p {
-  margin-left: 10px;
-}
-
-.gal-detail > div img {
-  border-radius: 10px;
-}
-
 </style>
